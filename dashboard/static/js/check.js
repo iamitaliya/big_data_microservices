69fe2fedbc3745048c0c3a8c5a73b8ca64f9428b
Nikhilkumar Italiya <nikhilkumar.italiya@st.ovgu.de>
2021-12-14 18:36:17 +0100

    update

--- dashboard/static/js/check.js
+++ dashboard/static/js/check.js
@@ -1,13 +1,13 @@
 ok_img = "static/images/ok.png"
 no_img = "static/images/no.png"
 wait_img = "static/images/wait.png"
-
+PASSWORD = "MOVIES"
 /*
     # visualiser microservice
     """
-    1. Collector:4321
-    2. Database:5432
-    3. Visualiser:8080
+    1. Collector:1111 4321
+    2. Database:1211:5432
+    3. Visualiser:1311 8080
     """
     # Recommender microservice
     """
@@ -33,6 +33,19 @@ function check_service(id){
     }, 400);
 }
 
+function update_services(id){
+    fetch(get_url(id.toString())).then(response => response.json()).then(res => {
+        // console.log("Update Request complete! response:", res.status);
+        if(res.status == "success"){ 
+            console.log("Update Request Successful");
+        }else{
+            console.log("Update Request Response ", res.status);
+        }
+    }).catch((error) => {
+            console.log("Update Request Failed");
+            console.log(error)
+        })
+}
 
 function change_img(id ,img){
     return document.getElementById(id).src = img
@@ -41,11 +54,11 @@ function change_img(id ,img){
 function get_url(id){
     switch(id){
         case "1":
-            return "http://localhost:4321/api/check-status"
+            return "http://localhost:1111/api/check-status"
         case "2":
             return "/status2"
         case "3":
-            return "http://localhost:8080/api/check-status"
+            return "http://localhost:1311/api/check-status"
         case "4":
             return "http://localhost:2111/api/check-status"
         case "5":
@@ -62,6 +75,13 @@ function get_url(id){
             return "http://localhost:3211/api/check-status"
         case "11":
             return "http://localhost:3311/api/check-status"
+        case "12":
+            return "http://localhost:1111/api/load-data/"+PASSWORD
+        case "13":
+            return "http://localhost:2111/api/load-data/"+PASSWORD
+        case "14":
+            return "http://localhost:3111/api/clean-data/"+ PASSWORD
+        
     }
 }
 
@@ -84,6 +104,7 @@ function check_status(id, url)
 
 function onload_check(){
     const services = [1,2,3,4,5,6,7,8,9,10,11]
+    const update_services = [12,13,14]
     services.forEach(function(item, i){
         check_service(item)
     })
@@ -93,4 +114,11 @@ function onload_check(){
             check_service(item)
         })
     }, 300000);
+
+    // it will update data every 24 Hours
+    var update_interval = window.setInterval(function(){
+        update_services.forEach(function(item, i){
+            update_service(item)
+        })
+    }, 86400000)
 }
